871ea4b9bec0f77fbcfcf5f9e5a60e5420e2362a
Surinder Kumar <skumar4sine@seattleu.edu>
2025-06-27 23:05:02 -0700

    refactor auth_required decorator to use default VALID_TOKENS

--- decorators/auth_required.py
+++ decorators/auth_required.py
@@ -1,11 +1,12 @@
 from flask import request, jsonify
+from config import VALID_TOKENS
 
 
-def auth_required(valid_tokens):
+def auth_required(tokens=VALID_TOKENS):
     def decorator(func):
         def wrapper(*args, **kwargs):
             token = request.headers.get("Authorization")
-            if not token or token not in valid_tokens:
+            if not token or token not in tokens:
                 return jsonify({"error": "Unauthorized"}), 401
             return func(*args, **kwargs)
 
