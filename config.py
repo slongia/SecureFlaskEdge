a23e249e5f44d838432f1e68c70b76948cb808cf
Surinder Kumar <skumar4sine@seattleu.edu>
2025-06-27 22:40:53 -0700

    config file

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+
+POSTGRESQL_CONFIG = {
+    "dbname": os.getenv("DB_NAME", "SecureFlaskEdge"),
+    "user": os.getenv("DB_USER", "postgres"),
+    "password": os.getenv("DB_PASSWORD", "postgres"),
+    "host": os.getenv("DB_HOST", "localhost"),
+    "port": os.getenv("DB_PORT", "5432"),
+}
+
+REDIS_URL = os.getenv("REDIS_URL", "redis://localhost:6379/0")
+VALID_TOKENS = {"secrettoken123"}
