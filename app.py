1ab2e56ef6c6676c1d81e24cda01f6dc544233ac
Surinder Kumar <skumar4sine@seattleu.edu>
2025-06-27 23:05:17 -0700

    refactor secure_data route to use default auth_required without VALID_TOKENS

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-from flask import Flask, request
+from flask import Flask
 from decorators.log_request import log_request
 from decorators.auth_required import auth_required
 from decorators.rate_limiter import rate_limiter
 
 app = Flask("SecureFlaskEdge")
 
-VALID_TOKENS = {"secrettoken123"}
-
 
 @app.route("/secure-data")
 @rate_limiter(max_calls=3, window_seconds=30)
-@auth_required(VALID_TOKENS)
+@auth_required()
 @log_request(prefix="SECURE")
 def secure_data():
     return {"status": "success", "data": "top secret info"}
@@ -22,10 +20,10 @@ def index():
     return "hello world"
 
 
-@app.route("/add/<int:a>/<int:b>", methods=["GET"])
-@log_request(prefix="MATH")
-def add(a, b):
-    return str(a + b)
+@app.route("/public", methods=["GET"])
+@log_request(prefix="PUBLIC")
+def public():
+    return "Public Access"
 
 
 if __name__ == "__main__":
