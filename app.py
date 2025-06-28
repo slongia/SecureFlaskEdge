0e21f532056f2215115979f5c1f243c4b0fa86c6
Surinder Kumar <skumar4sine@seattleu.edu>
2025-06-27 19:07:52 -0700

    auth, rate limit decorator

--- app.py
+++ app.py
@@ -1,7 +1,19 @@
 from flask import Flask, request
 from decorators.log_request import log_request
+from decorators.auth_required import auth_required
+from decorators.rate_limiter import rate_limiter
 
 app = Flask("SecureFlaskEdge")
+
+VALID_TOKENS = {"secrettoken123"}
+
+
+@app.route("/secure-data")
+@rate_limiter(max_calls=3, window_seconds=30)
+@auth_required(VALID_TOKENS)
+@log_request(prefix="SECURE")
+def secure_data():
+    return {"status": "success", "data": "top secret info"}
 
 
 @app.route("/", methods=["GET"])
@@ -17,4 +29,4 @@ def add(a, b):
 
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
