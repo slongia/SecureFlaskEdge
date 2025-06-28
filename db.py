ed30033d57951b32793ee7f57ebe4416bc339602
Surinder Kumar <skumar4sine@seattleu.edu>
2025-06-27 23:05:25 -0700

    refactor get_connection to use POSTGRESQL_CONFIG for database connection parameters

--- db.py
+++ db.py
@@ -1,12 +1,13 @@
 import psycopg2
-from psycopg2.extras import RealDictCursor
+from config import POSTGRESQL_CONFIG
 
 
 def get_connection():
     return psycopg2.connect(
-        dbname="SecureFlaskEdge",
-        user="postgres",
-        password="postgres",
-        host="localhost",
-        port="5432",
+        # dbname="SecureFlaskEdge",
+        # user="postgres",
+        # password="postgres",
+        # host="localhost",
+        # port="5432",
+        **POSTGRESQL_CONFIG
     )
